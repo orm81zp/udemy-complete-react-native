fb81aabb60a6d93fa42855c9a5e2aaa8d841d12c
Roman Onishchenko <orm81zp@gmail.com>
2020-02-18 06:41:31 +0200

    colors

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -22,6 +22,11 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Counter Demo"
         onPress={() => navigation.navigate('Counter')}
       />
+
+      <Button
+        title="Go to Color Demo"
+        onPress={() => navigation.navigate('Color')}
+      />
     </View>
   )
 };
