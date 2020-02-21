c4feab676681ed128b8a0755dde71395eaba9841
Roman Onishchenko <orm81zp@gmail.com>
2020-02-21 06:31:57 +0200

    square screen updates

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,31 +5,40 @@ import ColorCounter from '../components/ColorCounter';
 const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
+  const defaultColors = { red: 0, green: 0, blue: 0 };
+  const [colors, setColors] = useState(defaultColors);
+
+  const setColor = (color, change) => {
+    const sum = colors[color] + change;
+    if (sum > 255 || sum < 0) {
+      return;
+    }
+    setColors({
+      ...colors,
+      [color]: sum
+    })
+  }
+
+  const { red, green, blue } = colors;
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + COLOR_INCREMENT)}
-        onDecrease={() => setRed(red - COLOR_INCREMENT)}
-        color="Red" />
+        onIncrease={() => setColor('red', COLOR_INCREMENT )}
+        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
+        color="Red"
+      />
       <ColorCounter
-        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
-        color="Blue" />
+        onIncrease={() => setColor('blue', COLOR_INCREMENT )}
+        onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
+        color="Blue"
+      />
       <ColorCounter
-        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
-        color="Green" />
-
-        <Button onPress={() => {
-          setRed(0);
-          setGreen(0);
-          setBlue(0);
-        }} title="Reset colors"/>
-
+        onIncrease={() => setColor('green', COLOR_INCREMENT )}
+        onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
+        color="Green"
+      />
+      <Button onPress={() => setColors(defaultColors)} title="Reset colors"/>
       <View style={{
         height: 150,
         width: 150,
